6e9d7e376d406a169027b422bbbf77707f25a8a4
apearson1995 <apearson1995@berkeley.edu>
2016-04-02 08:57:06 -0700

    Edited things with Dvir
    Main things we need to do:
    0. Finish up Data Structures Portion
    1. Translate things to new notebook without answers
    2. Create Slides

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def test_prop_diff(func):
     if func("ATTGAC","ATGGCC") < .34 and func("ATTGAC","ATGGCC") > .32:
-        return "CONGRTULATIONS YOUR FUNCTION IS CORRECT :) !"
+        return "CONGRATULATIONS YOUR FUNCTION IS CORRECT :) !"
     else:
         return "You made a mistake, please try again"
 
